ed8aa25dcf68b12ec2f4513f37742b302c557e77
simon3397 <simon3397@gmail.com>
2024-02-08 23:26:43 +0900

    [BUG_FIX]

--- app/src/main/java/com/byurin/datepicker/MainActivity.kt
+++ app/src/main/java/com/byurin/datepicker/MainActivity.kt
@@ -12,14 +12,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    var textDate = binding.textDate
-    var buttonDate = binding.buttonDate
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val textDate = binding.textDate
+        val buttonDate = binding.buttonDate
 
         val calendarBox = Calendar.getInstance() // 현재 시간을 기준으로 달력 객체 생성
         val dateBox = DatePickerDialog.OnDateSetListener { datePicker, year, month, dayOfMonth -> // DatePickerDialog 의 날짜 설정되었을 때의 동작 정의 리스너
@@ -34,11 +34,12 @@ class MainActivity : AppCompatActivity() {
             DatePickerDialog(this, dateBox, calendarBox.get(Calendar.YEAR), calendarBox.get(Calendar.MONTH), calendarBox.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
+
     // 달력 객체에 설정된 날짜를 텍스트 형식으로 변환하여 텍스트뷰에 업데이트
     private fun updateText(calendar: Calendar){
         val dateFormat = "dd-MM-yyyy" // 날짜 출력 형식 지정
         val simple = SimpleDateFormat(dateFormat, Locale.KOREA) // SimpleDateFormat 사용하여 날짜 형식 지정
-        textDate.setText(simple.format(calendar.time)) // 지정한 날짜 형식으로 날짜를 문자열로 변환하여 텍스트뷰에 설정
+        binding.textDate.text = simple.format(calendar.time) // 지정한 날짜 형식으로 날짜를 문자열로 변환하여 텍스트뷰에 설정
     }
 }
 
